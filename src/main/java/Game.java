26e9c5febdb685a6a003dd511f26af6ac363bc6c
unknown <oliver.cavallo@educanet.cz>
2021-06-06 19:22:01 +0200

    update

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,30 +1,72 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
 public class Game {
     public static void main(String args[]) {
 
-        int boardArr[][] = new int[10][10];
+        char boardArr[][] = new char[10][10];
+        int choose = 0;
 
-        for (int row = 0; row < boardArr.length; row++) {
-            for (int col = 0; col < boardArr[row].length; col++) {
-                boardArr[row][col] = 0;
-                System.out.print(boardArr[row][col] + "\t");
-            }
-            System.out.println();
-        }
+        ArrayList<Player> players = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("Zadej radek");
-        int rowPos = sc.nextInt();
-        System.out.println("Zadej sloupec");
-        int colPos = sc.nextInt();
-        boardArr[rowPos][colPos] = 2;
 
+        System.out.println("1. Pridej hrace");
+        System.out.println("2. Zacni hru");
 
+        choose = sc.nextInt();
+
+        if(choose == 1){
+            System.out.println("Zadej jmeno hrace");
+            sc.nextLine();
+            String name = sc.nextLine();
+
+            System.out.println("Zadej symbol pro " + name);
+            char symbol = sc.next().charAt(0);
+            Player player = new Player(name, symbol);
+
+            players.add(player);
+
+            for (int playersInArraylist = 0; playersInArraylist < players.size(); playersInArraylist++){
+                System.out.println(players.get(playersInArraylist).toString());
+            }
+
+        } else if (choose == 2){
+            for (int row = 0; row < boardArr.length; row++) {
+                for (int col = 0; col < boardArr[row].length; col++) {
+                    boardArr[row][col] = '-';
+                    System.out.print(boardArr[row][col] + "\t");
+                }
+                System.out.println();
+            }
+
+            System.out.println("Zadej radek");
+            int rowPos = sc.nextInt() - 1;
+
+            System.out.println("Zadej sloupec");
+            int colPos = sc.nextInt() - 1;
+
+            boardArr[rowPos][colPos] = 'X';
+
+            for (int i = 0; i < boardArr.length; i++){
+                for (int j = 0; j < boardArr.length; j++){
+                    System.out.print(boardArr[i][j] + "\t");
+                }
+                System.out.println();
+            }
+
+        }
 
 
 
 
     }
+
+    /*@Override
+    public String toString(char boardArr[][]){
+        return boardArr[]
+    }*/
 }
---
+++ src/main/java/Player.java
@@ -0,0 +1,10 @@
+public class Player {
+
+    String name;
+    char symbol;
+
+    public Player(String name, char symbol ){
+        this.symbol = symbol;
+        this.name = name;
+    }
+}
